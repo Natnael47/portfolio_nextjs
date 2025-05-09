98b3db2dd48f7259e8fc62791509b566dbd3c788
Natnael47 <nati47m10@gmail.com>
2025-05-09 12:14:23 +0300

    #services section W/2

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { navItems } from "@/data";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 import TimeLine from "./components/TimeLine";
 
 export default function Home() {
@@ -12,7 +13,7 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <About />
-
+        <Services />
         <TimeLine />
         <Footer />
       </div>
